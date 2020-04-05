716f30509fdd29ec6661a8685b6ef6e31da90b86
Shadowmeth <47725754+Shadowmeth@users.noreply.github.com>
2020-04-06 02:46:13 +0500

    Add files via upload
    uploaded sqrt.py

---
+++ sqrt.py
@@ -0,0 +1,25 @@
+def abs(n):
+  if n < 0:
+    return n * -1
+  return n
+
+def square(x):
+  return x * x
+
+def is_good_enough(guess, n):
+  return abs((square(guess) - n)) > 0.0000000001
+
+# abs(guess * guess - n) > 0.0000000001
+
+def average(a, b):
+  return (a + b) / 2
+
+def improve_guess(guess, n):
+  return average (guess, n / guess)
+
+def sqrt(n):
+  guess = 1
+
+  while is_good_enough(guess, n):
+    guess = improve_guess(guess, n)
+  return guess
